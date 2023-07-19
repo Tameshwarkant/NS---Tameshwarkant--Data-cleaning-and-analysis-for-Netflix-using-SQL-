b02c50a77d0d8bd51ceab0c91aa87c6ca407406c
Tameshwar kant <125332848+Tameshwarkant@users.noreply.github.com>
2023-07-20 01:42:01 +0530

    Create Segment 8 - Recommendations for Content Strategy.sql

---
+++ Segment 8 - Recommendations for Content Strategy.sql
@@ -0,0 +1,10 @@
+-- Q1: -	Based on the analysis, provide recommendations for the types of content Netflix should focus on producing.
+-- Ans:- Based on the analysis, Netflix should focus on producing a diverse range of content that caters to various genres and categories. 
+--       This will help capture a wider audience and keep viewers engaged.
+--       Regularly monitoring and analyzing viewer feedback, ratings, and preferences can inform content decisions and help identify emerging trends or genres that are gaining popularity.
+
+
+-- Q2: -	Identify potential areas for expansion and growth based on the analysis of the dataset.
+-- Ans:- Expanding into new markets and exploring partnerships with international production companies can provide opportunities for growth and audience expansion.
+--       Investing in original content creation, including movies and TV shows, can differentiate Netflix from competitors and foster viewer loyalty.
+
